d3b37830beb3ccd66dd2d91791adfe48021b1472
tlchambers <thuylchambers@gmail.com>
2024-04-24 23:05:36 +0100

    adding func to add stars to background

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
+//Stars
+// Sets the number of stars we wish to display
+const numberOfStars = 100;
+
+// For every star we want to display
+for (let i = 0; i < numberOfStars; i++) {
+    let star = document.createElement("div");
+    star.className = "star";
+    const xy = getRandomPosition();
+    star.style.top = xy[0] + 'px';
+    star.style.left = xy[1] + 'px';
+    document.body.append(star);
+}
+
+// Gets random x, y values based on the size of the container
+function getRandomPosition() {  
+    var y = window.innerWidth;
+    var x = window.innerHeight;
+    var randomX = Math.floor(Math.random()*x);
+    var randomY = Math.floor(Math.random()*y);
+    return [randomX,randomY];
+  }
+
+
 // DOM manipulation
 const quoteToType = document.getElementById("quoteDisplay");
 
+
+// Bug - random num could be '0' meaning when api fetch 0 returns '404', starting num must be 1
 function generateRandomNumber() {
     return Math.floor(Math.random() * 6);
 }
+
 
 const randomNumber = generateRandomNumber();
 
@@ -20,7 +47,7 @@ const fetchApi = () => {
 };
 
 
-async function getNewQuote() {
+async function renderNewQuote() {
     try {
         const getQuote = await fetchApi();
         const newQuote = getQuote.result.properties.opening_crawl
@@ -32,4 +59,4 @@ async function getNewQuote() {
 }
 
 
-getNewQuote()
+renderNewQuote()
